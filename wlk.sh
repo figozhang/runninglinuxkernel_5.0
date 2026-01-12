79500794d28f7bb326923129f0297ba8e24d6593
lkwu <849416449@qq.com>
2026-01-12 20:05:51 +0800

    ENH: save compile and debug config for pve102_ubuntu
    Signed-off-by: lkwu <849416449@qq.com>

---
+++ wlk.sh
@@ -0,0 +1,259 @@
+#!/bin/bash
+LROOT=$PWD
+JOBCOUNT=${JOBCOUNT=$(nproc)}
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-linux-gnu-
+export INSTALL_PATH=$LROOT/rootfs_debian_arm64/boot/
+export INSTALL_MOD_PATH=$LROOT/rootfs_debian_arm64/
+export INSTALL_HDR_PATH=$LROOT/rootfs_debian_arm64/usr/
+kernel_build=$PWD/rootfs_debian_arm64/usr/src/linux/
+rootfs_path=$PWD/rootfs_debian_arm64
+rootfs_image=$PWD/rootfs_debian_arm64.ext4
+rootfs_size=2048
+SMP="-smp 4"
+QEMU=qemu-system-aarch64
+# QEMU=qemu-system-x86_64
+rootfs_arg="root=/dev/vda rootfstype=ext4 rw console=ttyAMA0,115200"
+kernel_arg="noinitrd nokaslr"
+crash_arg="crashkernel=256M"
+dyn_arg="vfio.dyndbg=+pflmt irq_gic_v3_its.dyndbg=+pflmt iommu.dyndbg=+pflmt irqdomain.dyndbg=+pflmt"
+debug_arg="loglevel=8 sched_debug"
+
+show_menu() {
+    echo "========================================"
+    echo "    ARM64 Kernel Development Tools"
+    echo "========================================"
+    echo "1) menuconfig     - Configure kernel"
+    echo "2) build_kernel   - Build kernel image"
+    echo "3) build_rootfs   - Build rootfs image (need root)"
+    echo "4) update_rootfs  - Update kernel modules (need root)"
+    echo "5) run            - Run QEMU system"
+    echo "6) run debug      - Run QEMU with GDB debug"
+    echo "7) run_test       - Run QEMU test mode"
+    echo "8) exit           - Exit"
+    echo "========================================"
+    echo -n "Please select an option [1-8]: "
+}
+
+if [ $# -eq 0 ]; then
+    while true; do
+        show_menu
+        read choice
+        case $choice in
+            1)
+                echo "Selected: menuconfig"
+                $0 menuconfig
+                ;;
+            2)
+                echo "Selected: build_kernel"
+                $0 build_kernel
+                ;;
+            3)
+                echo "Selected: build_rootfs"
+                $0 build_rootfs
+                ;;
+            4)
+                echo "Selected: update_rootfs"
+                $0 update_rootfs
+                ;;
+            5)
+                echo "Selected: run"
+                $0 run
+                ;;
+            6)
+                echo "Selected: run debug"
+                $0 run debug
+                ;;
+            7)
+                echo "Selected: run_test"
+                $0 run_test
+                ;;
+            8)
+                echo "Goodbye!"
+                exit 0
+                ;;
+            *)
+                echo "Invalid option. Please select 1-8."
+                ;;
+        esac
+        echo
+        echo "Press Enter to continue..."
+        read
+    done
+fi
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 [arg]"
+    echo "menuconfig: reconfig the kernel"
+    echo "build_kernel: build the kernel image."
+    echo "build_rootfs: build the rootfs image, need root privilege"
+    echo "update_rootfs: update kernel modules for rootfs image, need root privilege."
+    echo "run: run debian system."
+    echo "run debug: enable gdb debug server."
+fi
+if [ $# -eq 2 ] && [ $2 == "debug" ]; then
+    echo "Enable qemu debug server"
+    DBG="-s -S"
+fi
+make_kernel_image(){
+        echo "start build kernel image..."
+        make debian_defconfig
+        make -j $JOBCOUNT
+}
+make_menuconfig(){
+               echo "start re-config kernel"
+               make debian_defconfig
+               make menuconfig
+               cp .config $PWD/arch/arm64/configs/debian_defconfig
+}
+prepare_rootfs(){
+        if [ ! -d $rootfs_path ]; then
+            echo "decompressing rootfs..."
+            # split -d -b 80m rootfs_debian_arm64.tar.xz -- rootfs_debian_arm64.part 
+            cat rootfs_debian_arm64.part0* > rootfs_debian_arm64.tar.xz
+            tar -Jxf rootfs_debian_arm64.tar.xz
+        fi
+}
+build_kernel_devel(){
+    kernver="$(cat include/config/kernel.release)"
+    echo "kernel version: $kernver"
+    mkdir -p $kernel_build
+    rm rootfs_debian_arm64/lib/modules/$kernver/build
+    cp -a include $kernel_build
+    cp Makefile .config Module.symvers System.map vmlinux $kernel_build
+    mkdir -p $kernel_build/arch/arm64/
+    mkdir -p $kernel_build/arch/arm64/kernel/
+    cp -a arch/arm64/include $kernel_build/arch/arm64/
+    cp -a arch/arm64/Makefile $kernel_build/arch/arm64/
+    cp arch/arm64/kernel/module.lds $kernel_build/arch/arm64/kernel/
+    ln -s /usr/src/linux rootfs_debian_arm64/lib/modules/$kernver/build
+    # ln to debian linux-5.0/scripts
+    ln -s /usr/src/linux-kbuild/scripts rootfs_debian_arm64/usr/src/linux/scripts
+    #ln -s /usr/src/linux-kbuild/tools rootfs_debian_arm64/usr/src/linux/tools
+}
+check_root(){
+        if [ "$(id -u)" != "0" ];then
+            echo "superuser privileges are required to run"
+            echo "sudo ./run_debian_arm64.sh build_rootfs"
+            exit 1
+        fi
+}
+update_rootfs(){
+        if [ ! -f $rootfs_image ]; then
+            echo "rootfs image is not present..., pls run build_rootfs"
+        else
+            echo "update rootfs ..."
+            mkdir -p $rootfs_path
+            echo "mount ext4 image into rootfs_debian_arm64"
+            mount -t ext4 $rootfs_image $rootfs_path -o loop
+            make install
+            make modules_install -j $JOBCOUNT
+            #make headers_install
+            build_kernel_devel
+            umount $rootfs_path
+            chmod 777 $rootfs_image
+            rm -rf $rootfs_path
+        fi
+}
+build_rootfs(){
+        if [ ! -f $rootfs_image ]; then
+            make install
+            make modules_install -j $JOBCOUNT
+            # make headers_install
+            build_kernel_devel
+            echo "making image..."
+            dd if=/dev/zero of=$rootfs_image bs=1M count=$rootfs_size
+            mkfs.ext4 $rootfs_image
+            mkdir -p tmpfs
+            echo "copy data into rootfs..."
+            mount -t ext4 $rootfs_image tmpfs/ -o loop
+            cp -af $rootfs_path/* tmpfs/
+            umount tmpfs
+            chmod 777 $rootfs_image
+            rm -rf $rootfs_path
+        fi
+}
+run_qemu_debian(){
+        cmd="$QEMU -m 1024 -cpu max,sve=on,sve256=on -M virt,gic-version=3,its=on,iommu=smmuv3\
+            -nographic $SMP -kernel arch/arm64/boot/Image \
+            -append \"$kernel_arg $debug_arg $rootfs_arg $crash_arg $dyn_arg\"\
+            -drive if=none,file=$rootfs_image,format=raw,id=hd0\
+            -device virtio-blk-device,drive=hd0\
+            --fsdev local,id=kmod_dev,path=./kmodules,security_model=none\
+            -device virtio-9p-pci,fsdev=kmod_dev,mount_tag=kmod_mount\
+            $DBG"
+        echo "running:"
+        echo $cmd
+        eval $cmd
+}
+run_qemu_debian_test(){
+        cmd="$QEMU -m 1024 -cpu cortex-a710 -M virt,gic-version=3,its=on,iommu=smmuv3\
+            -nographic $SMP -kernel arch/arm64/boot/Image \
+            -append \"$kernel_arg $debug_arg $rootfs_arg $crash_arg $dyn_arg\"\
+            -drive if=none,file=$rootfs_image,format=raw,id=hd0\
+            -device virtio-blk-device,drive=hd0\
+            --fsdev local,id=kmod_dev,path=./kmodules,security_model=none\
+            -device virtio-9p-pci,fsdev=kmod_dev,mount_tag=kmod_mount\
+            -monitor telnet::4444,server,nowait\
+            $DBG"
+        echo "running:"
+        echo $cmd
+        eval $cmd
+}
+# qemu-system-aarch64 \
+#     -M virt,gic-version=3 \
+#     -cpu cortex-a57 \
+#     -smp 2 \
+#     -m 512 \
+#     -accel tcg \
+#     -kernel /home/lkwu/runninglinux/runninglinuxkernel_5.0/arch/arm64/boot/Image \
+#     -drive file=/home/lkwu/runninglinux/runninglinuxkernel_5.0/rootfs_debian_arm64.ext4,format=raw,if=virtio,cache=writeback \
+#     -append "root=/dev/vda rw console=ttyAMA0,115200" \
+#     -nographic
+case $1 in
+    build_kernel)
+        make_kernel_image
+        #prepare_rootfs
+        #build_rootfs
+        ;;
+    
+    menuconfig)
+        make_menuconfig
+        ;;
+    build_rootfs)
+        #make_kernel_image
+        check_root
+        prepare_rootfs
+        build_rootfs
+        ;;
+    update_rootfs)
+        update_rootfs
+        ;;
+    run)
+        if [ ! -f $LROOT/arch/arm64/boot/Image ]; then
+            echo "canot find kernel image, pls run build_kernel command firstly!!"
+            exit 1
+        fi
+        if [ ! -f $rootfs_image ]; then
+            echo "canot find rootfs image, pls run build_rootfs command firstly!!"
+            exit 1
+        fi
+        #prepare_rootfs
+        #build_rootfs
+        run_qemu_debian
+        ;;
+    run_test)
+        if [ ! -f $LROOT/arch/arm64/boot/Image ]; then
+            echo "canot find kernel image, pls run build_kernel command firstly!!"
+            exit 1
+        fi
+        if [ ! -f $rootfs_image ]; then
+            echo "canot find rootfs image, pls run build_rootfs command firstly!!"
+            exit 1
+        fi
+        #prepare_rootfs
+        #build_rootfs
+        DBG="-s -S"
+        run_qemu_debian_test
+        ;;
+esac
